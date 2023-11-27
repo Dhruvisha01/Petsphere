802c90955651cce7b3319d3af92e1e55abcc07d7
Dhruvisha Mondhe <63810182+Dhruvisha01@users.noreply.github.com>
2023-11-27 08:46:02 -0500

    Set up the node environment

---
+++ A3/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+    res.render("index"); // index refers to index.ejs
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
